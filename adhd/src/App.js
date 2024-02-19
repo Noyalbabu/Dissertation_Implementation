744cd000f861565e596c4ae063f42c68cd67bd84
Noyal Babu <noyalbabu02@gmail.com>
2024-02-19 21:10:30 +0000

    Adding adhd react app!

---
+++ adhd/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import PlayGame from './elements/game';
+import About from './elements/about';
+import NavPage from './elements/navPage';
+function App() {
+  return (
+    <div>
+        <NavPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ adhd/src/elements/navPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css';
+
+function NavPage() {
+        return (
+            <div >
+                <div className="heading">
+                    <h1 >ADHD</h1>
+                    <h2 >Attention Deficit Hyperactive Disorder</h2>
+                </div>
+                <div className="App">
+                    <a href="/game">Game</a>
+                </div>               
+            </div> 
+        );
+    }
+
+export default NavPage;
