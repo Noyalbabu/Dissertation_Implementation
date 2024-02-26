b300d850e75e137b17493db31f71a5253ddad4a5
Noyal Babu <noyalbabu02@gmail.com>
2024-02-26 21:20:36 +0000

    Adding scene and 3D model

--- adhd/src/elements/game.js
+++ adhd/src/elements/game.js
@@ -1,92 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import 'aframe';
-import {Entity, Scene} from 'aframe-react';
-
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import sky from '../Images/sky.jpg';
+import lake from '../assets/lake.glb';
  
-export default (props) => {
-    const [color, setColor] = useState('red');
-    const changeColor = () => {
-      const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-      setColor(randomColor);};
-    return (
-        <Scene>
-          <a-assets>
-            <img crossOrigin id="groundTexture" src="img/floor.jpg" />
-            <img crossOrigin id="skyTexture" src="img/sky.jpg" />
-          </a-assets>
+function Game() {
+  const loader = new GLTFLoader();
 
-          <Entity
-            primitive="a-plane"
-            src="#groundTexture"
-            rotation="-90 0 0"
-            height="100"
-            width="100"
-          />
-          <Entity primitive="a-light" type="ambient" color="#445451" />
-          <Entity
-            primitive="a-light"
-            type="point"
-            intensity="2"
-            position="2 4 4"
-          />
-          <Entity
-            primitive="a-sky"
-            height="2048"
-            radius="30"
-            src="#skyTexture"
-            theta-length="90"
-            width="2048"
-          />
-          <Entity particle-system={{ preset: 'snow', particleCount: 2000 }} />
-          <Entity
-            text={{ value: 'Concentrate on the color!', align: 'center' }}
-            position={{ x: 0, y: 2, z: -1 }}
-          />
+  loader.load(lake, (d) =>{
+    const entity = document.getElementById("lake");
+    entity.object3D.add(d.scene);
+  })
 
-          <Entity
-            id="box"
-            geometry={{ primitive: 'box' }}
-            material={{ color: color, opacity: 0.6 }}
-            animation__rotate={{
-                property: 'rotation',
-                dur: 2000,
-                loop: true,
-                to: '360 360 360'
-            }}
-            animation__scale={{
-                property: 'scale',
-                dir: 'alternate',
-                dur: 100,
-                loop: true,
-                to: '1.1 1.1 1.1'
-            }}
-            position={{ x: 0, y: 1, z: -3 }}
-            events={{ click: changeColor }}
-        >
-            <Entity
-                animation__scale={{
-                    property: 'scale',
-                    dir: 'alternate',
-                    dur: 100,
-                    loop: true,
-                    to: '2 2 2'
-                }}
-                geometry={{ primitive: 'box', depth: 0.2, height: 0.2, width: 0.2 }}
-                material={{ color: '#24CAFF' }}
-            />
-          </Entity>
-          <Entity primitive="a-camera">
-            <Entity
-              primitive="a-cursor"
-              animation__click={{
-                property: 'scale',
-                startEvents: 'click',
-                from: '0.1 0.1 0.1',
-                to: '1 1 1',
-                dur: 150
-              }}
-            />
-          </Entity>
-      </Scene>
-    );
+  return (
+    <a-scene>
+      <a-assets>
+      <img id="sky" src={sky} />
+      <a-asset-item id="lake-model" src={lake}></a-asset-item>
+      </a-assets>
+      <a-sky
+      color = "#FFFFFF"
+      material = "src: #sky"
+      rotation = "0 0 0"
+      >
+      </a-sky>
+      <a-entity id="camera" camera look-controls wasd-controls position="-1 0.6 -2"></a-entity>
+      <a-entity id ="lake" position ="-1 -1 -10" scale = "20 -20 -20"></a-entity>
+    </a-scene>
+  );
 }
+
+export default Game;
