d036b790c88f9b797983c21fb2aed646754df5ed
Noyal Babu <noyalbabu02@gmail.com>
2024-03-06 11:43:20 +0000

    model loading error solved. An animated bird is loading now.

--- adhd/src/elements/bird.js
+++ adhd/src/elements/bird.js
@@ -6,21 +6,20 @@ import bird from '../assets/love_birds_parrot.glb';
 import Prototypes from 'prop-types';
 
 function Bird({x, y, z}) {
-    const AFRAME = window.AFRAME;
+
     useEffect(() => {
         const loader = new GLTFLoader();
         const entity = document.getElementById('bird-entity');
     
         loader.load(bird, (gltf) => {
-          if (entity) {
-            entity.object3D.add(gltf.scene);
+        if (entity) {
             debugger;
-          }
+            entity.object3D.add(gltf.scene);
+        }
         });
       }, []);
-
     return (
-            <a-entity id="bird-entity" anime position={`${x} ${y} ${z}` }> </a-entity>  
+            <a-entity id="bird-entity" position={`${x} ${y} ${z}`} animation> </a-entity>  
     );
 }
 
